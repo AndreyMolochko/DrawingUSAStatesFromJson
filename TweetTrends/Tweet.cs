b0b8a3e8485e50ca1fa0fb2805adf79f97956a77
AndreyMolochko <Molochko_AG_16@student.grsu.by>
2018-03-16 19:25:44 +0300

    beginning

---
+++ TweetTrends/Tweet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TweetTrends
+{
+    class Tweet
+    {
+        String longitude;
+        String latitude;
+        String date;
+        String context;
+        String location;
+        Double averageSentiment;
+        public Tweet(String longitude,String latitude,String date, String context)
+        {
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.date = date;
+            this.context = context;
+        }
+        
+    }
+}
